47e3791f2ae834183f554eb13247d05dd1e3dc84
tofu345 <shammahayotunde1@gmail.com>
2025-05-22 16:30:05 +0100

    feat: waybar network, weather, powermode (not done)

---
+++ scripts/change_powermode.hs
@@ -0,0 +1,20 @@
+import System.Environment (getArgs)
+import System.Directory (listDirectory)
+import Data.Char (isDigit)
+import Control.Monad (forM_)
+
+main = do
+    -- assume all cores have same powermode
+    modes <- words <$> readFile "/sys/devices/system/cpu/cpu0/cpufreq/scaling_available_governors"
+    args <- getArgs
+    case args of
+        [mode] | mode `elem` modes -> changeTo mode
+        _ -> putStrLn "Err: "
+
+changeTo mode = do
+    let baseDir = "/sys/devices/system/cpu/"
+        path el = baseDir ++ el ++ "/cpufreq/scaling_governor"
+    contents <- listDirectory baseDir
+    forM_ (path <$> filter cpus contents) $ \f -> writeFile f mode
+    where
+    cpus el = length el > 3 && isDigit (el !! 3)
